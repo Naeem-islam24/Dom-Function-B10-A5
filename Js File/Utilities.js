842a7c09ed4f4af748a0740a9a94678d4c2ed556
naeem-islam24 <naeemislam421bd@gmail.com>
2024-09-25 12:24:49 +0600

    added code

--- Js File/Utilities.js
+++ Js File/Utilities.js
@@ -1,26 +1,71 @@
 function getInputFieldValueById(id) {
     const donation = document.getElementById(id).value;
-    // const donationConvert=parseFloat(donation);
     return donation;
 }
 
 
-function showSectionById(id){
+function showSectionById(id) {
 
     document.getElementById('donation-part').classList.add('hidden');
     document.getElementById('history-part').classList.add('hidden');
-  
+
     document.getElementById(id).classList.remove('hidden');
 
 
 }
 
-document.getElementById('donate-btn').addEventListener('click',function(){
+document.getElementById('donate-btn').addEventListener('click', function () {
 
     showSectionById('donation-part');
 })
 
-document.getElementById('history-btn').addEventListener('click',function(){
+document.getElementById('history-btn').addEventListener('click', function () {
 
     showSectionById('history-part');
 })
+
+// Button Color Changing when press
+document.getElementById('history-btn').addEventListener('click', function () {
+    document.getElementById('donate-btn').classList.remove('bg-lime-400');
+    document.getElementById('history-btn').classList.add('bg-lime-400');
+
+});
+document.getElementById('donate-btn').addEventListener('click', function () {
+    document.getElementById('donate-btn').classList.add('bg-lime-400');
+    document.getElementById('history-btn').classList.remove('bg-lime-400');
+
+
+});
+
+
+document.getElementById('goToPageButton').addEventListener('click', function () {
+    window.location.href = './blog.html';
+});
+
+document.getElementById('goBackButton').addEventListener('click', function () {
+    // window.history.back();  
+    window.location.href = 'index.html';
+});
+
+
+document.getElementById("dropdown-button").addEventListener("click", function () {
+    const menu = document.getElementById("dropdown-menu");
+    const arrow = document.getElementById("down-arrow3");
+
+    if (menu.style.display === "none" || menu.style.display === "") {
+
+        menu.style.display = "block";
+        arrow.classList.remove("fa-chevron-down");
+        arrow.classList.add("fa-chevron-up");
+    }
+    else {
+
+        menu.style.display = "none";
+        arrow.classList.remove("fa-chevron-up");
+        arrow.classList.add("fa-chevron-down");
+    }
+});
+
+
+
+
--- Js File/domwithjs.js
+++ Js File/domwithjs.js
@@ -1,6 +1,10 @@
 // First Donation part
 document.getElementById('donate-one').addEventListener('click', function () {
 
+    // For Current time
+    const now = new Date();
+    const currentDateTime = now.toLocaleString();
+
     const donation = getInputFieldValueById('input-one');
     const donationValue = parseFloat(donation);
     console.log(donationValue);
@@ -26,6 +30,7 @@ document.getElementById('donate-one').addEventListener('click', function () {
     div.innerHTML = ` 
         <h4 class="text-2xl px-4 py-2 font-medium">Donation Amount:</h4>
         <p class="px-4 pb-2 font-bold my-2">${donationValue} .Taka is donated for Flood Relief in Noakhali,Bangladesh.</p>
+        <p class="px-4 pb-2 font-bold my-2" >Time: ${currentDateTime}</p>
         `
     document.getElementById('history-container').appendChild(div);
 
@@ -37,6 +42,10 @@ document.getElementById('donate-one').addEventListener('click', function () {
 
 // Second Donation part
 document.getElementById('donate-two').addEventListener('click', function () {
+
+    // For Current time
+    const now = new Date();
+    const currentDateTime = now.toLocaleString();
 
     const donation = getInputFieldValueById('input-two');
     const donationValue = parseFloat(donation);
@@ -61,8 +70,9 @@ document.getElementById('donate-two').addEventListener('click', function () {
     div.classList.add('bg-yellow-300');
     div.classList.add('rounded-lg');
     div.innerHTML = ` 
-        <h4 class="text-2xl font-medium">Donation Amount:</h4>
-        <p>${donationValue} .Taka is donated for Flood Relief in Feni,Bangladesh.</p>
+        <h4 class="text-2xl px-4 py-2 font-medium">Donation Amount:</h4>
+        <p class="px-4 pb-2 font-bold my-2">${donationValue} .Taka is donated for Flood Relief in Feni,Bangladesh.</p>
+         <p class="px-4 pb-2 font-bold my-2" >Time: ${currentDateTime}</p>
         `
     document.getElementById('history-container').appendChild(div);
 
@@ -73,6 +83,10 @@ document.getElementById('donate-two').addEventListener('click', function () {
 
 // Third Donation part
 document.getElementById('donate-three').addEventListener('click', function () {
+
+    // For Current time
+    const now = new Date();
+    const currentDateTime = now.toLocaleString();
 
     const donation = getInputFieldValueById('input-three');
     const donationValue = parseFloat(donation);
@@ -97,11 +111,56 @@ document.getElementById('donate-three').addEventListener('click', function () {
     div.classList.add('bg-yellow-300');
     div.classList.add('rounded-lg');
     div.innerHTML = ` 
-        <h4 class="text-2xl p border-solid rounded-lg font-medium">Donation Amount:</h4>
-        <p>${donationValue} .Taka is donated for Injured in the Quota Movement.</p>
+        <h4 class="text-2xl px-4 py-2 border-solid rounded-lg font-medium">Donation Amount:</h4>
+        <p class="px-4 pb-2 font-bold my-2">${donationValue} .Taka is donated for Injured in the Quota Movement.</p>
+         <p class="px-4 pb-2 font-bold my-2" >Time: ${currentDateTime}</p>
         `
     document.getElementById('history-container').appendChild(div);
     document.getElementById('my-balance').innerText = myNewBalance;
 
 })
+
+
+// First donation button alert
+const donateButton = document.getElementById("donate-one");
+const donationAmountField = document.getElementById("input-one");
+
+donateButton.addEventListener("click", function () {
+    const donationAmount = donationAmountField.value;
+
+    if (donationAmount && donationAmount > 0) {
+        window.alert("Congrats! You are donating " + donationAmount + " BDT Successfully! ");
+    } else {
+        window.alert("Please enter a valid donation amount.");
+    }
+});
+
+// Second donation button alert
+const donateButton2 = document.getElementById("donate-two");
+const donationAmountField2 = document.getElementById("input-two");
+
+donateButton2.addEventListener("click", function () {
+    const donationAmount2 = donationAmountField2.value;
+
+    if (donationAmount2 && donationAmount2 > 0) {
+        window.alert("Congrats! You are donating " + donationAmount2 + " BDT Successfully! ");
+    } else {
+        window.alert("Please enter a valid donation amount.");
+    }
+});
+
+// Third donation button alert
+const donateButton3 = document.getElementById("donate-three");
+const donationAmountField3 = document.getElementById("input-three");
+
+donateButton3.addEventListener("click", function () {
+    const donationAmount3 = donationAmountField3.value;
+
+    if (donationAmount3 && donationAmount3 > 0) {
+        window.alert("Congrats! You are donating " + donationAmount3 + " BDT Successfully!");
+    } else {
+        window.alert("Please enter a valid donation amount.");
+    }
+});
+
 
